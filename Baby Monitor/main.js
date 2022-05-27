b843a3615a239ce64c7dbc8e43bd29617309fbbb
VivaanRawal <80296406+VivaanRawal@users.noreply.github.com>
2022-05-27 16:47:07 -0700

    Add files via upload

---
+++ Baby Monitor/main.js
@@ -0,0 +1,52 @@
+img="";
+objects=[];
+status="";
+
+function setup(){
+    canvas=createCanvas(380,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status: Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Model Loadded");
+    status=true;
+   
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+
+}
+function draw(){
+    image(video,0,0,380,380);
+    if(status!=""){
+        r=random(255);
+        b=random(255);
+        g=random(255);
+        objectDetector.detect(video,gotResult);
+        for(i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML="Status: Objects Detected";
+           
+            fill(r,g,b);
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+ percent+ "%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            if(objects[i].label=="person"){
+                document.getElementById("number_of_objects").innerHTML="baby has been found";
+            }
+            else{
+                document.getElementById("number_of_objects").innerHTML="baby has not been found";
+            }
+        }
+    }
+   
+}
